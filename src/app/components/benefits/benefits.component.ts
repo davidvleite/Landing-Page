3b60c372a4472e5ebcef7da10957f058d7af5f5a
David Veríssimo Leite <davidd.verissimo@gmail.com>
2024-02-10 21:27:04 -0300

    Commit final
    Inclusão dos componentes:
    * Benefits
    * Footer

---
+++ src/app/components/benefits/benefits.component.ts
@@ -0,0 +1,17 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'benefits-card',
+  standalone: true,
+  imports: [
+    NgOptimizedImage,
+  ],
+  templateUrl: './benefits.component.html',
+  styleUrl: './benefits.component.scss'
+})
+export class BenefitsComponent {
+  @Input("img-path") imgPath: string = '';
+  @Input("card-title") cardTitle: string = '';
+  @Input("card-description") cardDescription: string = '';
+}
